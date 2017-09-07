77ac9fd86fd56748441b121f8d72d472ec22a397
PatrickHennis <patrickhennis@gmail.com>
2017-09-07 15:55:11 -0400

    started work on compiler

--- src/ccc/CompositionCompiler.h
+++ src/ccc/CompositionCompiler.h
@@ -7,6 +7,39 @@
 
 
 class CompositionCompiler {
+public:
+    auto IP = "([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})";
+    auto STR = "([A-Z|a-z]+[0-9]*)";
+    auto STR2 = "(%+[0-9]*)*";
+    auto Sn = IP + STR2 + "_(java|python|cpp):" + STR + ":" + STR;
+    auto RStmt = Sn + "(," + Sn + ")*" + "((\\+&?" + Sn + ")*|(\\+" + Sn + "(," + Sn + ")*)*)";
+    auto sCond = Sn + "(==|!=)\"" + STR + "\"";
+    auto cCond = sCond + "((&&|!!)" + sCond + ")*";
+    auto Cond = "((\\}?if|\\}elseif)\\(" + cCond + "\\)\\{" + RStmt + ")|(\\}else\\{" + RStmt + ")";
+
+    // vector containing instructions (java linked hash set)
+
+    std::string myServiceName;
+
+    CompositionCompiler(std::string service) {
+        myServiceName = service;
+    }
+
+    virtual ~CompositionCompiler() {}
+
+    // throws ClaraException
+    void compile(std::string iCode) {
+
+        // reset();
+
+        // use method call 'noBlanks' on iCode
+        std::string pCode = iCode;
+
+
+    }
+
+private:
+
 
 };
 
